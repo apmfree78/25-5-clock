6bb297a0c9a131c20d7e4717ce6aa6c51b2c0182
apmfree78 <amit@profitswami.com>
2022-03-28 11:30:49 -0400

    refactored with modern React with hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,52 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import Settings from './Settings';
+
 //setting initial states
 const _break = 5;
 const _session = 25;
 const _playing = false;
 const _current = 25 * 60;
-const _mode = "Session";
+const _mode = 'Session';
 
 //take seconds and returns mm:ss format as string
 const displayTime = (seconds) =>
   `${Math.floor(seconds / 60)
     .toString()
-    .padStart(2, "0")}:${(seconds % 60).toString().padStart(2, "0")}`;
+    .padStart(2, '0')}:${(seconds % 60).toString().padStart(2, '0')}`;
 
-class App extends React.Component {
-  state = {
+const App = () => {
+  /* state variables
     breakLength: _break, //break length in minutes
     sessionLength: _session, //session length in minutes
     isPlaying: _playing, //is clock on? boolean play => true, pause = false
     currentTime: _current, //current time in seconds
     mode: _mode, // OR 'Break' are we on break or in a session?
-  };
+  }; */
+  const [length, setLength] = useState({ break: _break, session: _session });
+  const [isPlaying, setIsPlaying] = useState(_playing);
+  const [currentTime, setCurrentTime] = useState(_current);
+  const [mode, setMode] = useState(_mode);
 
   //setting empty function to use hold setInterval return value
-  interval = () => {};
+  function interval() {}
 
   //callback function for setInternal
   //function will be repeated each second
   //this function decrements currentTime by 1 and changes mode when
   //currentTime hits zero
-  countDown = () => {
-    const { breakLength, sessionLength } = this.state;
-    let { currentTime, mode } = this.state;
+  const countDown = () => {
+    let _currentTime = currentTime;
+    let currentMode = mode;
 
     //reducing currentTime
-    currentTime -= 1;
+    _currentTime -= 1;
 
     // checking if current mode, break or session is complete
-    if (currentTime < 0) {
+    if (_currentTime < 0) {
       //switching mode and resetting currentTime to reflect mode change
-      if (mode == "Session") {
-        mode = "Break";
-        currentTime = breakLength * 60;
+      if (currentMode == 'Session') {
+        currentMode = 'Break';
+        _currentTime = breakLength * 60;
       } else {
-        mode = "Session";
-        currentTime = sessionLength * 60;
+        currentMode = 'Session';
+        _currentTime = sessionLength * 60;
       }
       //RING ALARM now that session has changed
-      const alarm = new Audio("household_alarm_clock_beep_tone.mp3");
+      const alarm = new Audio('household_alarm_clock_beep_tone.mp3');
 
       //setting volume
       alarm.volume = 0.5;
@@ -54,129 +60,102 @@ class App extends React.Component {
     }
 
     //finally setting state
-    this.setState({ currentTime, mode });
+    // this.setState({ currentTime, mode });
+    setCurrentTime(_currentTime);
+    setMode(currentMode);
   };
 
-  playTimer = () => {
+  const playTimer = () => {
     //checking timer is running
-    if (!this.state.isPlaying) {
+    if (!isPlaying) {
       //only starting play, if timer was stopped
       //set isPlaying to true
-      this.setState({ isPlaying: true });
+      setIsPlaying(true);
 
       //starting timer using CountDown method
-      this.interval = setInterval(this.countDown, 1000);
+      interval = setInterval(countDown, 1000);
     }
   };
 
-  stopTimer = () => {
+  const stopTimer = () => {
     //checking timer is running
-    if (this.state.isPlaying) {
+    if (isPlaying) {
       //only stopping timer if it's running!!
 
       //set isPlaying to false
-      this.setState({ isPlaying: false });
+      setIsPlaying(false);
 
       //stop timer
-      clearInterval(this.interval);
+      clearInterval(interval);
     }
   };
 
   // reset state to initial values
-  refresh = () => {
+  const refresh = () => {
     //if timer is running, stop it
     //set isPlaying to false
-    if (this.state.isPlaying) {
+    if (isPlaying) {
       //only stopping timer if it's running!!
 
       //set isPlaying to false
-      this.setState({ isPlaying: false });
+      setIsPlaying(false);
 
       //stop timer
-      clearInterval(this.interval);
+      clearInterval(interval);
     }
-    this.setState({
-      breakLength: _break, //break length in minutes
-      sessionLength: _session, //session length in minutes
-      isPlaying: _playing, //is clock on? boolean play => true, pause = false
-      currentTime: _current, //current time in seconds
-      mode: _mode, // OR 'Break' are we on break or in a session?
-    });
+    setLength({ break: _break, session: _session }); //break & session length in minutes
+    setIsPlaying(_playing); //is clock on? boolean play => true, pause = false
+    setCurrentTime(_current); //current time in seconds
+    setMode(_mode); // OR 'Break' are we on break or in a session?
   };
 
-  setBreakLength = (length) => {
+  const changeLength = ({ breaklength, sessionlength }) => {
     // making sure new breakLength is within boundries
     //AND timer is not running
-    let { mode, currentTime } = this.state;
-    if (length <= 60 && length > 0 && !this.state.isPlaying) {
-      const breakLength = length;
-      if (mode == "Break") currentTime = breakLength * 60;
-      this.setState({ currentTime, breakLength });
-    }
-  };
-  setSessionLength = (length) => {
-    // making sure new breakLength is within boundries
-    //AND timer is not running
-    let { mode, currentTime } = this.state;
-    if (length <= 60 && length > 0 && !this.state.isPlaying) {
-      const sessionLength = length;
-      if (mode == "Session") currentTime = sessionLength * 60;
-      this.setState({ currentTime, sessionLength });
+    let _currentTime = currentTime;
+    let currentMode = mode;
+
+    //make sure timer is paused
+    if (!isPlaying) {
+      //making sure new break or session length setting are
+      //over 0 minutes and less than 60 minutes
+      if (
+        breaklength < 60 &&
+        sessionlength < 60 &&
+        breaklength > 0 &&
+        sessionlength > 0
+      ) {
+        if (mode == 'Break') _currentTime = breaklength * 60;
+        else if (mode == 'Session') _currentTime = sessionlength * 60;
+
+        //setting state
+        setLength({ break: breaklength, session: sessionlength });
+      }
     }
   };
 
-  render() {
-    const { breakLength, sessionLength, isPlaying, currentTime, mode } =
-      this.state;
+  return (
+    <div id='clock'>
+      <h1>25+5 Clock</h1>
+      <div id='settings' className='d-flex justify-content-around'>
+        <Settings length={length} changeLength={changeLength}>
+          Break
+        </Settings>
+        <Settings length={length} changeLength={changeLength}>
+          Session
+        </Settings>
+      </div>
+      <div id='timer'>
+        <div id='timer-label'>{mode}</div>
+        <div id='time-left'>{displayTime(currentTime)}</div>
+      </div>
+      <div id='controls'>
+        <i id='start' className='fa fa-play fa-2x' onClick={playTimer} />
+        <i id='stop' className='fa fa-pause fa-2x' onClick={stopTimer} />
+        <i id='reset' className='fa fa-refresh fa-2x' onClick={refresh} />
+      </div>
+    </div> /* id=clock */
+  );
+};
 
-    return (
-      <div id='clock'>
-        <h1>25+5 Clock</h1>
-        <div id='settings' className='d-flex justify-content-around'>
-          <div className='length'>
-            <h3 id='break-label'>Break Length</h3>
-            <i
-              className='fa fa-arrow-up fa-lg'
-              id='break-increment'
-              onClick={() => this.setBreakLength(breakLength + 1)}
-            />
-            <span id='break-length'>{breakLength}</span>
-            <i
-              className='fa fa-arrow-down fa-lg'
-              id='break-decrement'
-              onClick={() => this.setBreakLength(breakLength - 1)}
-            />
-          </div>
-          <div className='length'>
-            <h3 id='session-label'>Session Length</h3>
-            <i
-              className='fa fa-arrow-up fa-lg'
-              id='session-increment'
-              onClick={() => this.setSessionLength(sessionLength + 1)}
-            />
-            <span id='session-length'>{sessionLength}</span>
-            <i
-              className='fa fa-arrow-down fa-lg'
-              id='session-decrement'
-              onClick={() => this.setSessionLength(sessionLength - 1)}
-            />
-          </div>
-        </div>
-        <div id='timer'>
-          <div id='timer-label'>{mode}</div>
-          <div id='time-left'>{displayTime(currentTime)}</div>
-        </div>
-        <div id='controls'>
-          <i id='start' className='fa fa-play fa-2x' onClick={this.playTimer} />
-          <i id='stop' className='fa fa-pause fa-2x' onClick={this.stopTimer} />
-          <i
-            id='reset'
-            className='fa fa-refresh fa-2x'
-            onClick={this.refresh}
-          />
-        </div>
-      </div> /* id=clock */
-    );
-  }
-}
 export default App;
---
+++ src/components/Settings.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Settings = ({ length, changeLength, children }) => {
+  const mode = children;
+
+  //determine what setting to display
+  const display = mode === 'Break' ? length.break : length.session;
+
+  const changebyAmount = (amount) => {
+    if (mode === 'Break') {
+      return {
+        breaklength: length.break + amount,
+        sessionlength: length.session,
+      };
+    } else if (mode === 'Session') {
+      return {
+        breaklength: length.break,
+        sessionlength: length.session + amount,
+      };
+    }
+  };
+  return (
+    <>
+      <div className='length'>
+        <h3 id='{mode}-label'>{mode} Length</h3>
+        <i
+          className='fa fa-arrow-up fa-lg'
+          id='{mode}-increment'
+          onClick={() => changeLength(changebyAmount(1))}
+        />
+        <span id='{mode}-length'>{display}</span>
+        <i
+          className='fa fa-arrow-down fa-lg'
+          id='{mode}-decrement'
+          onClick={() => changeLength(changebyAmount(-1))}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Settings;
